a7300f7f52ab723557bb37f8ee813deb70659543
dikyayodihamzah <dikyayodihamzah@gmail.com>
2023-09-06 10:47:20 +0700

    chore: added fiber error handler

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"log"
 
+	"github.com/dikyayodihamzah/connex-bot/config"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/gofiber/fiber/v2"
 )
-
-// Your Telegram Bot API Token
-const botToken = "YOUR_BOT_TOKEN"
 
 // The chat ID of the group or channel where you want to broadcast the message
 const chatID = "@your_channel_or_group"
@@ -16,10 +14,7 @@ const chatID = "@your_channel_or_group"
 func main() {
 	app := fiber.New()
 
-	bot, err := tgbotapi.NewBotAPI(botToken)
-	if err != nil {
-		log.Fatal(err)
-	}
+	bot := config.NewBot()
 
 	app.Post("/broadcast", func(c *fiber.Ctx) error {
 		messageText := c.Query("message")
